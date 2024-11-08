93c00650a9edc048b46246a6fe944189c076b2c7
Yuzheng Liu <z5598864@ad.unsw.edu.au>
2024-11-08 15:50:26 +1100

    2.1.0 welcome page

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,20 @@
+// src/Home.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Presto</h1>
+      <p>Please login or register to continue</p>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+      <Link to="/register">
+        <button>Register</button>
+      </Link>
+    </div>
+  );
+}
+
+export default Home;
